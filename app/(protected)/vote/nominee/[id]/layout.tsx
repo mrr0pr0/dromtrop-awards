8e204dc6614dc37dabb87cbbd60102925f066736
mrr0pr0 <167307326+mrr0pr0@users.noreply.github.com>
2026-06-05 09:30:38 +0200

    fixed dubble headr

--- app/(protected)/vote/nominee/[id]/layout.tsx
+++ app/(protected)/vote/nominee/[id]/layout.tsx
@@ -1,18 +1,7 @@
-import { Navbar } from '@/components/layout/navbar';
-import { Footer } from '@/components/layout/footer';
-
 export default function NomineeDetailLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	return (
-		<div className="min-h-screen bg-black text-white">
-			<Navbar />
-			<main className="mx-auto w-full max-w-[1180px] flex-1 px-4 py-8 pb-20 md:py-10">
-				{children}
-			</main>
-			<Footer />
-		</div>
-	);
+	return <>{children}</>;
 }
